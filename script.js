0161d3fe4838c6c27631eb635fb7faadf560aef9
Ulises Sosa <ulisesdevsosa@gmail.com>
2023-01-11 17:34:23 -0300

    Create 4 basic functions to calculate the total
    Make these functions work with only two values at a time
    Create a calculate functions that takes one operator and two numbers
    and performs the requested operation.

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log('it works');
+function add (){
+    let total = 0;
+    for (let i = 0; i < arguments.length; i++){
+        total += Math.round(arguments[i] *100) / 100;
+    }
+    console.log(total);
+    return total;
+}
+
+function sub (a, b) {
+    let total = Math.round((a-b)*100)/100;
+    console.log(total);
+    return total;
+}
+
+function mult(a, b) {
+    let total = Math.round((a*b)*100)/100;
+    console.log(total);
+    return total;
+}
+
+function div(a, b) {
+    let total = Math.round((a/b)*100)/100;
+    console.log(total);
+    return total;
+}
+
+function calculate(operator, num1, num2){
+    if(operator === '+'){
+        return add(num1, num2);
+    }else if (operator === '-'){
+        return sub(num1, num2);
+    }else if (operator === '*'){
+        return mult(num1, num2);
+    }else if (operator === '/'){
+        return div(num1, num2);
+    }
+}
+
+calculate('/', 5, 7);
