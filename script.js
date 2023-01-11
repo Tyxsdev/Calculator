4f61b7e29ed8b86bf82b8475ac5855a27583f32c
Ulises Sosa <ulisesdevsosa@gmail.com>
2023-01-11 20:03:06 -0300

    Select all buttons and the display
    Arrange the buttons in different arrays to be used later
    Set one eventlistener to clear the display of the dummy text
    when any button is press
    Set one eventlistener to update the display textContent with the
    textContent of the button pressed. Exept the equal button and the
    clear button.

--- script.js
+++ script.js
@@ -37,4 +37,61 @@ function calculate(operator, num1, num2){
     }
 }
 
-calculate('/', 5, 7);
+/* const one = document.querySelector('.file-three :nth-child(1)');
+const two = document.querySelector('.file-three :nth-child(2)');
+const three = document.querySelector('.file-three :nth-child(3)');
+const four = document.querySelector('.file-two :nth-child(1)');
+const five = document.querySelector('.file-two :nth-child(2)');
+const six = document.querySelector('.file-two :nth-child(3)');
+const seven = document.querySelector('.file-one :nth-child(1)');
+const eigth = document.querySelector('.file-one :nth-child(2)');
+const nine = document.querySelector('.file-one :nth-child(3)');
+const divideSymbol = document.querySelector('.file-four :nth-child(1)');
+const zero = document.querySelector('.file-four :nth-child(2)');
+const multSymbol = document.querySelector('.file-four :nth-child(3)');
+const equalSymbol = document.querySelector('.first button');
+const addSymbol = document.querySelector('.second button'); 
+const subSymbol = document.querySelector('.third button');   */
+const wrapper = document.querySelector('.wrapper')
+const display = document.querySelector('.display')
+const buttons = document.querySelectorAll('button');
+const allButtonsArray = Array.from(buttons);
+const buttonsToDisplay = [...allButtonsArray.slice(0, 12), ...allButtonsArray.slice(13, 15)];
+const numberButtons = [...allButtonsArray.slice(0, 9), ...allButtonsArray.slice(10, 11)];
+const symbolButtons = [...buttonsToDisplay.slice(9,10), ...buttonsToDisplay.slice(11,)];
+let displayValue = display.textContent;
+
+
+function setDisplay(e){
+    if(buttonsToDisplay.includes(e.target)){
+        display.textContent = null;
+        display.textContent += e.target.textContent;
+        displayValue = display.textContent;
+    } else {
+        display.textContent = null;
+        displayValue = display.textContent;
+    }
+    
+}
+
+function updateDisplay(e){
+    display.textContent += e.target.textContent;
+    displayValue = display.textContent;
+}
+
+
+buttonsToDisplay.forEach(button => {
+    button.addEventListener('click', updateDisplay)
+})
+
+wrapper.addEventListener('click', setDisplay, {once: true});
+
+console.log(displayValue);
+
+
+
+
+
+
+
+
