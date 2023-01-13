a05a93c8b02af7df21b57a6f6308fc488031749a
Ulises Sosa <ulisesdevsosa@gmail.com>
2023-01-13 15:31:23 -0300

    Modify the functions inside the events
    Make the calculator work when a negative value is in the first place
    Display an error message when trying to divide by zero
    Modify the dummy text

--- script.js
+++ script.js
@@ -21,8 +21,9 @@ function mult(a, b) {
 
 function div(a, b) {
     let total = Math.round((a/b)*100)/100;
-    if (total === Infinity){
-        return `Error`;        
+    if (b === 0){
+        alert(`Something's wrong i can feel it`)
+        return clearDisplay();        
     } else{
         return total;
     }
@@ -44,27 +45,27 @@ function calculate(operator, num1, num2){
     }
 }
 
-const wrapper = document.querySelector('.wrapper')
-const display = document.querySelector('.display')
+const wrapper = document.querySelector('.wrapper');
+const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('button');
 const allButtonsArray = Array.from(buttons);
 const buttonsToDisplay = [...allButtonsArray.slice(0, 12), ...allButtonsArray.slice(13, 15)];
 const numberButtons = [...allButtonsArray.slice(0, 9), ...allButtonsArray.slice(10, 11)];
 const symbolButtons = [...buttonsToDisplay.slice(9,10), ...buttonsToDisplay.slice(11,)];
-const equal = document.querySelector('.first button')
-const clear = document.querySelector('.container-three button')
-let displayValue = 0;
+const equal = document.querySelector('.first button');
+const clear = document.querySelector('.container-three button');
 let symbolOperator = '';
 let firstValue;
 let secondValue = 0;
 let arrayOfValues = [];
 let arrayOfValuesWhitoutSpace = [];
 let arrayOfSymbols = [];
+let fullArray = [];
 
 
 
 function setDisplay(e){
-    if(buttonsToDisplay.includes(e.target)){
+    if(numberButtons.includes(e.target) || e.target.textContent === '-'){
         display.textContent = null;
         display.textContent = e.target.textContent;          
     } else {
@@ -86,40 +87,54 @@ numberButtons.forEach(button => {
 
 wrapper.addEventListener('click', setDisplay, {once: true});
 
+
 function updateWithSymbol(e){
-    let buttonClick = `${e.target.textContent}`;
+    let buttonClick = `${e.target.textContent}`;    
     display.textContent += e.target.textContent;
-    arrayOfValues = display.textContent.split(/[^\w.]|_/g);
-    arrayOfValuesWhitoutSpace = arrayOfValues.filter(char => {
-        if (char === '') return false;
-        return true;
-    })
-    arrayOfSymbols = display.textContent
-        .split('')
-        .filter(char => char !== '')
-        .filter(char => char === '+' || char === '-' || char === '*' || char === '/');
-    console.log(arrayOfValuesWhitoutSpace);
-    if (arrayOfValuesWhitoutSpace.length){
-        for (let i = 0; i < arrayOfValuesWhitoutSpace.length -1; i++){
-            console.log(arrayOfValuesWhitoutSpace.length);
-            if (firstValue === undefined){
-                firstValue = arrayOfValuesWhitoutSpace[i];
-            } else if (firstValue < 0){
-                let symbolsNegative = [...arrayOfSymbols.slice(1,)];
-                console.log(`symbolsNegative ${symbolsNegative}`);
-                secondValue = parseFloat(arrayOfValuesWhitoutSpace[i+1]);                
-                symbolOperator = symbolsNegative[symbolsNegative.length - 1];
-                console.log(`negative operator = ${symbolOperator}`);
-                firstValue = display.textContent = calculate(symbolOperator, firstValue, secondValue);
-                display.textContent += `${buttonClick}`;    
-            } else{
-                secondValue = parseFloat(arrayOfValuesWhitoutSpace[i+1]);
-                console.log(`firstValue = ${firstValue}`);
-                console.log(`secondValue = ${secondValue}`);
-                symbolOperator = arrayOfSymbols[i]
-                console.log(`symbol = ${symbolOperator}`);
-                firstValue = display.textContent = calculate(symbolOperator, firstValue, secondValue);
-                display.textContent += `${buttonClick}`;         
+    fullArray = display.textContent.split('')
+    if ((fullArray[0] === '*' || fullArray [0] === '/' ||  fullArray [0] === '+' ) && fullArray.length === 1){
+        alert(`Please enter a valid number as first value`);
+        clearDisplay();
+    } else {   
+        arrayOfValues = display.textContent.split(/[^\w.]|_/g);
+        arrayOfValuesWhitoutSpace = arrayOfValues.filter(char => {
+            if (char === '') return false;
+            return true;
+        })
+        arrayOfSymbols = display.textContent
+            .split('')
+            .filter(char => char !== '')
+            .filter(char => char === '+' || char === '-' || char === '*' || char === '/');
+        console.log(arrayOfValuesWhitoutSpace);    
+        if (arrayOfValuesWhitoutSpace.length){
+            for (let i = 0; i < arrayOfValuesWhitoutSpace.length -1; i++){                
+                if (firstValue === undefined){
+                    firstValue = arrayOfValuesWhitoutSpace[i];
+                } else if (firstValue < 0){
+                    let symbolsNegative = [...arrayOfSymbols.slice(1,)];
+                    console.log(`symbolsNegative ${symbolsNegative}`);
+                    secondValue = parseFloat(arrayOfValuesWhitoutSpace[i+1]);                
+                    symbolOperator = symbolsNegative[0];
+                    console.log(`negative operator = ${symbolOperator}`);
+                    firstValue = display.textContent = calculate(symbolOperator, firstValue, secondValue);
+                    display.textContent += `${buttonClick}`;    
+                } else if (fullArray[0] ===  '-'){
+                    let symbolsNegative = [...arrayOfSymbols.slice(1,)];
+                    firstValue = firstValue * (-1)
+                    secondValue = parseFloat(arrayOfValuesWhitoutSpace[i+1]);                
+                    symbolOperator = symbolsNegative[0];
+                    console.log(`negative operator = ${symbolOperator}`);
+                    firstValue = display.textContent = calculate(symbolOperator, firstValue, secondValue);
+                    display.textContent += `${buttonClick}`;
+                } else{
+                    secondValue = parseFloat(arrayOfValuesWhitoutSpace[i+1]);
+                    console.log(`firstValue = ${firstValue}`);
+                    console.log(`secondValue = ${secondValue}`);
+                    symbolOperator = arrayOfSymbols[i]
+                    console.log(`symbol = ${symbolOperator}`);
+                    firstValue = display.textContent = calculate(symbolOperator, firstValue, secondValue);
+                    display.textContent += `${buttonClick}`;         
+                }
             }
         }
     }
@@ -140,6 +155,8 @@ symbolButtons.forEach(button => {
 
 
 function grabSecond(e){  
+    fullArray = display.textContent.split('');
+    console.log(`aaaaaa ${fullArray}`);
     arrayOfValues = display.textContent.split(/[^\w.]|_/g);
     arrayOfValuesWhitoutSpace = arrayOfValues.filter(char => {
         if (char === '') return false;
@@ -148,18 +165,18 @@ function grabSecond(e){
     arrayOfSymbols = display.textContent
         .split('')
         .filter(char => char !== '')
-        .filter(char => char === '+' || char === '-' || char === '*' || char === '/');
-    console.log(arrayOfValues);
-    if (arrayOfValues.length > 1 && firstValue === undefined){
+        .filter(char => char === '+' || char === '-' || char === '*' || char === '/');  
+    if (arrayOfValues.length > 1 && fullArray[0] != '-'){
         firstValue = parseFloat(arrayOfValuesWhitoutSpace[0]);
         secondValue = parseFloat(arrayOfValuesWhitoutSpace[1]);
         symbolOperator = arrayOfSymbols[1] || arrayOfSymbols[0];
         firstValue = display.textContent = calculate(symbolOperator, firstValue, secondValue);
-    }else if(arrayOfValues.length > 1){
+    }else if(fullArray[0] === '-'){
+        firstValue = (parseFloat(arrayOfValuesWhitoutSpace[0])) * (-1);
         secondValue = parseFloat(arrayOfValuesWhitoutSpace[1]);
         symbolOperator = arrayOfSymbols[1] || arrayOfSymbols[0];
         firstValue = display.textContent = calculate(symbolOperator, firstValue, secondValue);
-    }
+    } 
     else clearDisplay();    
 } 
 
